b2ddfffa1b64c115d52395537ae8008525b1e751
Kathryn Burruss <burruss.km@gmail.com>
2018-05-27 16:35:44 -0700

    initial commit

---
+++ postalcode_audit.py
@@ -0,0 +1,29 @@
+OSMFILE = "SLC_map.osm"
+
+postalcode_re = re.compile(r'^[8]\d{4}(?:[-\s]\d{4})?$')
+
+def is_postalcode(elem):
+    """Check if elem is postcode"""
+    return (elem.attrib['k'] == "addr:postcode" or elem.attrib['k'] == "postal_code")
+
+def audit_postalcode(postalcodes,value):
+    """Audit the format of value and return unexpecteed value"""
+    m = postalcode_re.search(value)
+    if not m: #if the value is not a postcode
+        postalcodes.add(value)
+
+def audit(osmfile):
+    """Return expected postcode values"""
+    osm_file = open(osmfile, "r")
+    postalcodes = set()
+    for event, elem in ET.iterparse(osm_file, events=("start",)):
+        if elem.tag == "node" or elem.tag == "way": #only check way and node
+            for tag in elem.iter("tag"):
+                if is_postalcode(tag):
+                    audit_postalcode(postalcodes,tag.attrib['v'])
+    osm_file.close()
+    return postalcodes
+
+postalcodes = audit(OSMFILE)
+print "unexpected postal codes:"
+pprint.pprint(postalcodes)
---
+++ streetname_audit.py
@@ -0,0 +1,56 @@
+OSMFILE = "SLC_map.osm"
+
+# evaluates and matches the last word in the street name
+street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
+zip_code_re = re.compile(r'^\d{5}(?:[-\s]\d{4})?$')
+
+expected = ["Street", "Avenue", "Boulevard", "Drive", "Court", "Circle", "Place", "Square", "Lane", "Road",
+            "Trail", "Parkway", "Commons", "North", "East", "South", "West", "Temple", "Way", "Terrace"]
+
+mapping = { "St": "Street",
+            "St.": "Street",
+            "Rd.": "Road",
+            "Rd": "Road",
+            "Ave.": "Avenue",
+            "Ave": "Avenue",
+            "Dr": "Drive",
+            "Dr.": "Drive",
+            "W": "West",
+            "S": "South",
+            "W.": "West"
+          }
+
+# evaluate if the tag is the street address
+def is_street_name(elem):
+    return (elem.attrib['k'] == "addr:street")
+
+def audit_street_type(street_types, street_name):
+    m = street_type_re.search(street_name)
+    if m:
+        street_type = m.group()
+        if street_type not in expected:
+            street_types[street_type].add(street_name)
+
+def audit(osmfile):
+    osm_file = open(osmfile, "r")
+    street_types = defaultdict(set)
+    for event, elem in ET.iterparse(osm_file, events=("start",)):
+
+        if elem.tag == "node" or elem.tag == "way":
+            for tag in elem.iter("tag"):
+                if is_street_name(tag):
+                    audit_street_type(street_types, tag.attrib['v'])
+    osm_file.close()
+    return street_types
+
+# this function is to update the street names to what's specified in the mapping dict, e.g. Ave.-->Avenue
+def update_name(name, mapping):
+    for search_error in mapping:
+        if search_error in name:
+            name = re.sub(r'\b' + search_error + r'\b\.?', mapping[search_error], name)
+
+    return name
+
+
+st_types = audit(OSMFILE)
+pprint.pprint(dict(st_types))
