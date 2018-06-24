bd86132e8fb9c0323566557ce1c2a4764eecbe82
Kathryn Burruss <burruss.km@gmail.com>
2018-06-24 13:10:54 -0700

    removed imports and moved postcode set up in code

--- postalcode_audit.py
+++ postalcode_audit.py
@@ -1,12 +1,10 @@
 import xml.etree.cElementTree as ET
 import pprint
-import os
 import re
-from collections import defaultdict
-import pprint
 
 # OpenStreetMaps file
 OSMFILE = "SLC_map.osm"
+postalcodes = set()
 
 #regex to check the formatting of postal codes
 postalcode_re = re.compile(r'^8\d{4}-?(\d{4})?$')
@@ -19,13 +17,11 @@ def audit_postalcode(postalcodes,value):
     """Audit the format of value and return unexpecteed value"""
     m = postalcode_re.search(value)
     if not m: #if the value is not a postcode
-    # is this where I would add to the problematics list?
         postalcodes.add(value)
 
 def audit(osmfile):
     """Return expected postcode values"""
     osm_file = open(osmfile, "r")
-    postalcodes = set()
     for event, elem in ET.iterparse(osm_file, events=("start",)):
         if elem.tag == "node" or elem.tag == "way": #only check way and node
             for tag in elem.iter("tag"):
